ba77f9c97c74acc59b26621a21784275b70e394c
CuongDo <cuongdoleviet@gmail.com>
2019-11-20 14:17:47 +0700

    modified to controllers MVC

---
+++ controllers/user.controller.js
@@ -0,0 +1,39 @@
+var db = require('../lowdb')
+var shortid = require('shortid');
+
+module.exports.index = function(req, res) {
+	res.render('users/index', {
+		users: db.get('users').value()
+	})
+}
+
+module.exports.search = function(req, res) {
+	// req.query lay cac parameters duoc truyen vao tu link vi du users/search?q=Cuong
+	var searchValue = req.query.searchValue 
+	var matchedUsers = db.get('users').value().filter(function(user){
+		return user.name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
+	})
+	res.render('users/index', {
+		users: matchedUsers,
+		searchValue : searchValue
+	})
+}
+
+module.exports.create = function (req, res) {
+	res.render('users/create')
+}
+
+module.exports.userDetail =  function(req, res){
+	var id = req.params.id
+	var user = db.get('users').find({id : id}).value()
+	res.render('users/userDetail',{
+		user: user
+	})
+}
+
+module.exports.postCreate = function(req, res){
+	var id = shortid.generate()
+	req.body.id = id
+	db.get('users').push(req.body).write()
+	res.redirect('/users') // redirect den trang users sau khi post
+}
--- route/user.route.js
+++ route/user.route.js
@@ -1,44 +1,16 @@
 var express = require('express')
-var shortid = require('shortid');
+var controller = require('../controllers/user.controller')
+
 var router = express.Router()
-var db = require('../lowdb')
 
+router.get('/', controller.index )
 
-router.get('/', function(req, res) {
-	res.render('users/index', {
-		users: db.get('users').value()
-	})
-})
+router.get('/search', controller.search )
 
-router.get('/search', function(req, res) {
-	// req.query lay cac parameters duoc truyen vao tu link vi du users/search?q=Cuong
-	var searchValue = req.query.searchValue 
-	var matchedUsers = db.get('users').value().filter(function(user){
-		return user.name.toLowerCase().indexOf(searchValue.toLowerCase()) !== -1
-	})
-	res.render('users/index', {
-		users: matchedUsers,
-		searchValue : searchValue
-	})
-})
+router.get ('/create', controller.create )
 
-router.get ('/create', function (req, res) {
-	res.render('users/create')
-})
+router.get('/:id',controller.userDetail)
 
-router.get('/:id', function(req, res){
-	var id = req.params.id
-	var user = db.get('users').find({id : id}).value()
-	res.render('users/userDetail',{
-		user: user
-	})
-})
-
-router.post('/create', function(req, res){
-	var id = shortid.generate()
-	req.body.id = id
-	db.get('users').push(req.body).write()
-	res.redirect('/users') // redirect den trang users sau khi post
-})
+router.post('/create', controller.postCreate)
 
 module.exports = router
