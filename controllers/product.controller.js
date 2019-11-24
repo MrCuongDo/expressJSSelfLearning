a2c8ad72e471db41b69d380de377adce48698849
CuongDo <cuongdoleviet@gmail.com>
2019-11-24 23:35:16 +0700

    add pagination into products page

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,7 +3,18 @@ var db = require('../lowdb')
 
 
 module.exports.index = function(req, res) {
+	var page = req.query.page || 1;  // cach viet ngan gon, neu khong co page thi mac dinh la 1
+	var perPage = 8; // x
+
+	var start = (page-1)*perPage;
+	var end = page * perPage;
+
+	var numberOfProducts = db.get('products').value().length;
+	var totalNumberOfPage = Math.round(numberOfProducts / perPage);
+
 	res.render('products/index', {
-		products: db.get('products').value()
+		products: db.get('products').value().slice(start,end),
+		currentPage : page,
+		totalNumberOfPage: totalNumberOfPage
 	})
 }
